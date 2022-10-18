cc467687ae4dc63dc636b097a4d8fa1a4bc4b78b
willycoxy <williamjwcox@gmail.com>
2022-10-17 21:00:03 -0400

    inital setup

---
+++ README.md
@@ -0,0 +1,18 @@
+# TECH BLOG
+
+## Description 
+CMS-style blog site, where you can publis articles, blog posts and thoughts and opinions.
+
+## Built With
+* HTML5
+* CSS
+* JavaScript
+* Boostrap
+* Socket.io
+* Handlebars.js
+* Node.js
+
+## Contributors 
+* Will
+
+## Website
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const routes = require("./controllers");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'willgod',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+        });
